daf3e211c4a2247efb1c4187edad4d4d599b3208
Nishant Patel <nishantuwindsor@gmail.com>
2017-12-23 18:30:00 -0500

    Fix BookLoader class

--- app/src/main/java/com/example/android/booklist/BookLoader.java
+++ app/src/main/java/com/example/android/booklist/BookLoader.java
@@ -2,11 +2,12 @@ package com.example.android.booklist;
 
 import android.content.Context;
 import android.content.AsyncTaskLoader;
-import android.util.Log;;
+import android.util.Log;
 
 import java.util.List;
 
 /**
+ * Loader class for loading book data in background thread
  * Created by Nishant on 9/6/2017.
  */
 
@@ -15,8 +16,7 @@ public class BookLoader extends AsyncTaskLoader<List<Book>> {
     private String mUrl;
     private String LOG_TAG = BookLoader.class.getName();
 
-
-    public BookLoader(Context context, String url) {
+    BookLoader(Context context, String url) {
         super(context);
         mUrl = url;
     }
@@ -27,7 +27,6 @@ public class BookLoader extends AsyncTaskLoader<List<Book>> {
         forceLoad();
     }
 
-
     @Override
     public List<Book> loadInBackground() {
 
@@ -37,9 +36,6 @@ public class BookLoader extends AsyncTaskLoader<List<Book>> {
             return null;
         }
 
-        List<Book> currentBookData = QueryUtils.fetchBookInformation(mUrl);
-        //Log.i(LOG_TAG,"\n load in background"+mCurrentBookData.toString());
-        return currentBookData;
+        return QueryUtils.fetchBookInformation(mUrl);
     }
-
 }
